adeaa1b17bbbe1b5418d9a15522e30a7e1273599
dagthomas <dagthomas@gmail.com>
2018-11-24 13:37:05 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# BGR Bysykkel  v1.0.0
+
+### Default view ('/'):
+![alt text](https://raw.githubusercontent.com/dagthomas/bgrsykkel/master/readme/default_site.jpg "Default Site")
+
+### List view ('/liste'):
+![alt text](https://raw.githubusercontent.com/dagthomas/bgrsykkel/master/readme/list_of_stations.jpg "List of Stations")
+
+### Stations view ('/stations/xxx-xxx-xxx'):
+![alt text](https://raw.githubusercontent.com/dagthomas/bgrsykkel/master/readme/multiple_stations.jpg "Selected Stations")
+
+### Multiple Stations view ('/stations/xxx-xxx-xxx-xxx-xxx-xxx'):
+![alt text](https://raw.githubusercontent.com/dagthomas/bgrsykkel/master/readme/multiple_stations.jpg "Multiple Stations")
+
+### List view, select ('/liste'):
+![alt text](https://raw.githubusercontent.com/dagthomas/bgrsykkel/master/readme/select.jpg "Select")
+
+## Installation
+
+```shell
+npm install
+npm run start
+```
+
+## Why BGR Bysykkel?
+
+Oslo Bysykkel gives users the opportunity to rent bikes on a daily, or monthy basis.<br> 
+This project was made to show info about the close, surrounding stations to our offices <br>
+In order the make traveling more effective.<br>
+
+Since oslobysykkel.no API does not allow CORS - I download the .json data via CURL.<br>
+
+ * Show all locations, with ID
+ * Show default view of stations surrounding our offices
+ * Show view based on user input
+ <br>
---
+++ routes/index.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var exec = require('node-exec-promise').exec;
+var router = express.Router();
+var fs = require('fs');
+
+// Get client-identifier from https://developer.oslobysykkel.no/api
+let id = "Client-Identifier:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", url = "https://oslobysykkel.no/api/v1/stations/availability";
+
+router.get('/stasjoner/:stasjoner', function(req, res, next) {
+var avail = JSON.parse(fs.readFileSync('avail.json', 'utf8'));
+var stations = JSON.parse(fs.readFileSync('stations.json', 'utf8'));
+	exec(
+		'curl -H '+id+' '+url
+	)
+		.then(
+			function(out) {
+        fs.writeFile('avail.json', out.stdout, 'utf8', function readFileCallback(err) {});
+        avail = JSON.parse(out.stdout);
+			},
+			function(err) {
+				console.error(err);
+			}
+		)
+		.then(function() {
+			var stasjonsId = req.params.stasjoner.split('-');
+
+			var station = [];
+			for (var i = 0, len = stasjonsId.length; i < len; i++) {
+        if(i < 6){
+        station[i] = stations.find(item => item.id === parseInt(stasjonsId[i]));
+        }
+      }
+			for (var i = 0, len = stasjonsId.length; i < len; i++) {
+        if(i < 6){
+          station[i].status = avail.stations.find(item => item.id === parseInt(stasjonsId[i]));
+        }
+			}
+			res.render('index', {
+        title: 'BYSYKKEL',
+        stat: station,
+        liste: null
+			});
+		}).then(function() {
+
+      
+    });
+});
+router.get('/', function(req, res, next) {
+  var avail = JSON.parse(fs.readFileSync('avail.json', 'utf8'));
+  var stations = JSON.parse(fs.readFileSync('stations.json', 'utf8'));
+	exec(
+		'curl -H '+id+' '+url
+	)
+		.then(
+			function(out) {
+        fs.writeFile('avail.json', out.stdout, 'utf8', function readFileCallback(err) {});
+        avail = JSON.parse(out.stdout);
+			},
+			function(err) {
+				console.error(err);
+			}
+		)
+		.then(function() {
+			var defaultStations = ['302', '354', '392'];
+			var station = [];
+			for (var i = 0, len = defaultStations.length; i < len; i++) {
+        if(i < 6){
+        station[i] = stations.find(item => item.id === parseInt(defaultStations[i]));
+        }
+      }
+			for (var i = 0, len = defaultStations.length; i < len; i++) {
+        if(i < 6){
+          station[i].status = avail.stations.find(item => item.id === parseInt(defaultStations[i]));
+        }
+			}
+			res.render('index', {
+        title: 'BYSYKKEL',
+        stat: station,
+        liste: null
+			});
+		});
+});
+router.get('/liste', function(req, res, next) {
+  var stations = JSON.parse(fs.readFileSync('stations.json', 'utf8'));
+	res.render('liste', {
+    title: 'BYSYKKEL', 
+    stat: null,
+    liste: stations
+	});
+});
+router.post('/getstasjoner', function(req, res) {
+	console.log("clicked");
+	var stasjoner = req.body.idliste.replace(/\s+/g, '').replace(/,/g, '-');
+  res.redirect(req.baseUrl + '/stasjoner/'+stasjoner);
+});
+module.exports = router;
